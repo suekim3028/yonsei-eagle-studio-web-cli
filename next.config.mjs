e162f1d415b6534ed50237b7c6acdb0f6c3c17f2
Suebin Kim <k.sue3028@gmail.com>
2024-04-08 21:05:09 +0900

    feat: configure styled components in compiler

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@suekime3028/web-core"], //Next.js can automatically transpile and bundle dependencies from local packages
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
