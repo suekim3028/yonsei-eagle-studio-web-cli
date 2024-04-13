1579a668320d334ec72cd6e70d4c5e7447382475
Suebin Kim <k.sue3028@gmail.com>
2024-04-14 04:33:33 +0900

    style: modify body css for mobile only

---
+++ src/app/styles.ts
@@ -0,0 +1,9 @@
+"use client";
+import styled from "styled-components";
+
+export const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  min-height: 100vh;
+`;
