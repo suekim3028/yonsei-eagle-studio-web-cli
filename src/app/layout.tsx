15f36df60bb290e32fff554763ebeb575705e121
Suebin Kim <k.sue3028@gmail.com>
2024-04-28 05:05:21 +0900

    feat: add manifest file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className={inter.className}>
         <StyledComponentsRegistry>
           <ThemeProvider>
