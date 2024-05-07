ccea28339c33497f4c6e4225aac99ef886a34142
Suebin Kim <k.sue3028@gmail.com>
2024-05-08 00:46:22 +0900

    feat: initialize api

--- src/components/app/Initializer/Initializer.tsx
+++ src/components/app/Initializer/Initializer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { WebPushManager } from "@lib";
+import { API } from "@web-core";
 import { useEffect } from "react";
 
 const Initializer = ({ children }: { children: React.ReactNode }) => {
@@ -14,6 +15,9 @@ const Initializer = ({ children }: { children: React.ReactNode }) => {
     if (!WebPushManager.initialized) {
       WebPushManager.initialize();
     }
+    API.init({
+      baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT_URL,
+    });
   }, []);
 
   return children;
--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -2,4 +2,5 @@ export type EnvConfig = {
   NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY: string;
   NEXT_PUBLIC_VAPID_PUBLIC_KEY: string;
   NEXT_PUBLIC_VAPID_PRIVATE_KEY: string;
+  NEXT_PUBLIC_API_ENDPOINT_URL: string;
 };
