9ace4cc18a021a91a556775abf6f7f76f767683c
Suebin Kim <k.sue3028@gmail.com>
2024-05-09 13:07:03 +0900

    feat: remove console in production

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@suekime3028/web-core"], //Next.js can automatically transpile and bundle dependencies from local packages
+
   compiler: {
     styledComponents: true,
+    removeConsole: process.env.NODE_ENV === "production",
   },
 };
 
