6fad578c241364c5cd7ff7ee8602d0c211f5d89b
Suebin Kim <k.sue3028@gmail.com>
2024-05-12 00:09:02 +0900

    feat: complete call image upload and request photo flow

--- src/app/generate/components/steps/UploadingPhotos/UploadingPhotos.tsx
+++ src/app/generate/components/steps/UploadingPhotos/UploadingPhotos.tsx
@@ -1,15 +1,71 @@
+import { photoApis } from "@apis";
 import { useStepContext } from "@app/generate/StepContext";
+import { photoRequestState } from "@atoms";
 import { Flex, Text } from "@components";
 import { WebPushManager } from "@lib";
-import { commonHooks, jsUtils } from "@web-core";
+import { commonHooks } from "@web-core";
+import { useRef } from "react";
+import { useSetRecoilState } from "recoil";
 
 const UploadingPhotos = () => {
-  const { goNext } = useStepContext();
+  const { goNext, photos, goPrev } = useStepContext();
 
+  const setPhotoRequest = useSetRecoilState(photoRequestState);
+
+  const uploadPhotos = async () => {
+    const photoIds = await Promise.all(
+      photos.map(
+        (photo) =>
+          new Promise(async (resolve: (photoId: string) => void, reject) => {
+            const { data: imageIdData, isError: linkIdError } =
+              await photoApis.getPhotoLinkId();
+            if (linkIdError) {
+              reject();
+              return;
+            }
+
+            const formData = new FormData();
+            formData.append("Filedata", photo);
+
+            const { isError: photoUploadError } = await photoApis.uploadPhoto({
+              imageId: imageIdData.imageId,
+              data: formData,
+            });
+
+            if (!photoUploadError) reject();
+
+            resolve(imageIdData.imageId);
+          })
+      )
+    );
+
+    return photoIds;
+  };
+
+  const handleError = () => {
+    // TODO: 에러 보여주기
+    goPrev("UPLOADING_PHOTOS");
+  };
+
+  const firstRender = useRef(false);
   commonHooks.useAsyncEffect(async () => {
-    //TODO: api 붙이기
-    await Promise.all([jsUtils.wait(3), WebPushManager.initialize()]);
-    goNext("UPLOADING_PHOTOS");
+    if (firstRender.current) return;
+    firstRender.current = true;
+
+    WebPushManager.initialize();
+    const photoIds = await uploadPhotos();
+    const { isError: uploadPhotoError } = await photoApis.createPhotoRequest({
+      imageList: photoIds,
+      imageProcessType: "NORMAL",
+    });
+
+    if (uploadPhotoError) return handleError();
+
+    const { data: photoRequest, isError: requestPhotoError } =
+      await photoApis.getPhotoRequest();
+    if (requestPhotoError) return handleError();
+
+    setPhotoRequest(photoRequest);
   }, []);
 
   return (
