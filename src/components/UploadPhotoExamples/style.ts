b8b8dfac5c4a06a9edb9b9f54803d861d15b509d
Suebin Kim <k.sue3028@gmail.com>
2024-05-15 01:20:33 +0900

    style: remove scroll bar

---
+++ src/components/UploadPhotoExamples/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const ScrollWrapper = styled.div`
+  scrollbar-width: none;
+  display: flex;
+  overflow-x: scroll;
+  padding: 0px 28px;
+  max-width: 100%;
+  align-self: center;
+
+  &::-webkit-scrollbar {
+    width: 0px;
+    height: 0px;
+    display: none;
+  }
+  &::-webkit-scrollbar-thumb {
+    display: none;
+  }
+`;
