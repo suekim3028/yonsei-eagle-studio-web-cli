2e7e555570125477c3be67735a582dac104c9ad1
Suebin Kim <k.sue3028@gmail.com>
2024-05-15 02:35:51 +0900

    feat: switch on request status

--- src/app/generate/components/Result/Result.tsx
+++ src/app/generate/components/Result/Result.tsx
@@ -1,14 +1,35 @@
+"use client";
+
 import { useStepContext } from "@app/generate/StepContext";
+import { Flex, Text } from "@components";
 import { PhotoTypes } from "@types";
 import React from "react";
+import Completed from "./templates/Completed";
 import Processing from "./templates/Processing";
 const Result = ({ request }: { request: PhotoTypes.Request }) => {
-  const { style } = useStepContext();
+  const {} = useStepContext();
 
   const render = () => {
     switch (request.requestStatus) {
       case "PROCESSING":
         return <Processing request={request} />;
+      case "COMPLETED":
+        return request.resultImage ? (
+          <Completed resultImage={request.resultImage} />
+        ) : (
+          <Processing request={request} />
+        );
+      case "ERROR":
+        return (
+          <Flex w="100%" p={20}>
+            <Text type={"16_Medium_Multi"}>
+              예상하지 못한 에러가 발생했어요. 혹시 이 화면을 보셨다면 카카오톡
+              채널로 꼭 저희에게 알려주세요!
+            </Text>
+          </Flex>
+        );
+      case "NOT_REQUESTED":
+        return <></>;
     }
   };
 
