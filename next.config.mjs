ce8a883541106fd8af68080d6f3f67cc31113515
Suebin Kim <k.sue3028@gmail.com>
2024-05-26 18:17:37 +0900

    feat: show log only in dev

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@suekime3028/web-core'], //Next.js can automatically transpile and bundle dependencies from local packages
-
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
