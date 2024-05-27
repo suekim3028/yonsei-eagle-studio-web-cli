dc7ce2d9203f822dd4739493b4f645fb49747f1c
Suebin Kim <k.sue3028@gmail.com>
2024-05-28 05:38:06 +0900

    feat: change lottie for loading before result

--- src/app/generate/components/Result/components/LoadingBeforeResult.tsx
+++ src/app/generate/components/Result/components/LoadingBeforeResult.tsx
@@ -1,27 +1,27 @@
-'use client';
+"use client";
 
-import { BgContainer, Flex, Text } from '@components';
-import { Player } from '@lottiefiles/react-lottie-player';
-import Camera from '@public/lottie/camera.json';
-import React from 'react';
+import { BgContainer, Flex, Text } from "@components";
+import { Player } from "@lottiefiles/react-lottie-player";
+import Loading from "@public/lottie/loading.json";
+import React from "react";
 
 const LoadingBeforeResult = () => {
   return (
     <BgContainer>
       <Flex
-        direction={'column'}
+        direction={"column"}
         flex={1}
-        alignItems={'center'}
-        justifyContent={'center'}
+        alignItems={"center"}
+        justifyContent={"center"}
       >
-        <Text type={'16_Light_Multi'} color={'YONSEI_NAVY'} mb={20}>
+        <Text type={"16_Light_Multi"} color={"YONSEI_NAVY"} mb={20}>
           두근두근, 어떤 모습일까요? 🦅
         </Text>
         <Player
           autoplay
           loop
-          src={Camera}
-          style={{ width: '280px', height: '264px' }}
+          src={Loading}
+          style={{ width: "48px", height: "48px" }}
         />
       </Flex>
     </BgContainer>
