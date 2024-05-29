e9527bbd9c3cd464e02a70df463101912a6ebdad
Suebin Kim <k.sue3028@gmail.com>
2024-05-29 23:15:05 +0900

    feat: change og

--- src/app/generate/components/Result/utils.ts
+++ src/app/generate/components/Result/utils.ts
@@ -4,17 +4,17 @@ import { webUtils } from "@web-core";
 import { addMinutes, differenceInSeconds } from "date-fns";
 
 export const copyLink = async () => {
-  const url = `${process.env.NEXT_PUNEXT_PUBLIC_SHARE_WEB_URLBLIC_WEB_URL}/generate`;
+  const url = `${process.env.NEXT_PUBLIC_SHARE_WEB_URL}/generate`;
 
   const canShare =
     "canShare" in navigator &&
     navigator.canShare({
-      text: `아카라카 기념 나만의 AI 프로필 확인하러 가기!\n${`${process.env.NEXT_PUBLIC_SHARE_WEB_URL}/generate`}`,
+      text: `무악대동제 기념 나만의 AI 프로필 확인하러 가기!\n${`${process.env.NEXT_PUBLIC_SHARE_WEB_URL}/generate`}`,
     });
   if (canShare) {
     try {
       await navigator.share({
-        text: `아카라카 기념 나만의 AI 프로필 확인하러 가기!\n${`${process.env.NEXT_PUBLIC_SHARE_WEB_URL}/generate`}`,
+        text: `무악대동제 기념 나만의 AI 프로필 확인하러 가기!\n${`${process.env.NEXT_PUBLIC_SHARE_WEB_URL}/generate`}`,
       });
     } catch (e) {
       console.warn("canceled");
