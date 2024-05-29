b45c448af8f326be2e08dda0666a1a42de5d8a98
Suebin Kim <k.sue3028@gmail.com>
2024-05-30 00:46:45 +0900

    chore: remove console

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   transpilePackages: ["@suekime3028/web-core"], //Next.js can automatically transpile and bundle dependencies from local packages
   compiler: {
-    // removeConsole: process.env.NODE_ENV === 'production',
+    removeConsole: process.env.NODE_ENV === "production",
   },
   reactStrictMode: true,
   images: {
