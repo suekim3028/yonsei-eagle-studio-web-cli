8f204b304c42c1ae87cbc45e514d68bba3bdc3f1
Suebin Kim <k.sue3028@gmail.com>
2024-05-29 23:44:39 +0900

    chore: show log

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@suekime3028/web-core'], //Next.js can automatically transpile and bundle dependencies from local packages
+  transpilePackages: ["@suekime3028/web-core"], //Next.js can automatically transpile and bundle dependencies from local packages
   compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
+    // removeConsole: process.env.NODE_ENV === 'production',
   },
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'kr.object.ncloudstorage.com',
+        protocol: "https",
+        hostname: "kr.object.ncloudstorage.com",
       },
     ],
   },
